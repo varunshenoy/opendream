51f0cea8547c5a6d1d67c8ee0c75c26508817ec7
Varun Shenoy <varun.nshenoy@gmail.com>
2023-05-13 13:50:18 -0700

    added inpaint code, but doesn't quite work yet

--- opendream/opendream.py
+++ opendream/opendream.py
@@ -13,6 +13,22 @@ def mask_and_inpaint(mask_and_inpaint_func):
     # FLESH OUT PARAM CHECKING, THIS NEEDS TO MATCH SO EXEC ENGINE CAN RUN
     functions["mask_and_inpaint"] = mask_and_inpaint_func
 
+def make_dummy_mask():
+    from PIL import Image, ImageDraw
+
+    # Create a blank mask with the size of 512x512
+    width, height = 512, 512
+    mask = Image.new("1", (width, height))
+
+    # Draw a simple shape on the mask using ImageDraw
+    draw = ImageDraw.Draw(mask)
+    draw.rectangle([128, 128, 384, 384], fill="white")
+    
+    mask.save(f"mask_inpaint.png")
+                
+    return mask
+
+
 def execute(json_file_path: str):
     with open(json_file_path) as json_file:
         data = json.load(json_file)
@@ -22,7 +38,7 @@ def execute(json_file_path: str):
             if task["operation"] == "dream":
                 current_image = functions["dream"](task["prompt"], **task["params"])
             elif task["operation"] == "mask_and_inpaint":
-                current_image = functions["mask_and_inpaint"](task["mask"], data["image"])
+                current_image = functions["mask_and_inpaint"](make_dummy_mask(), current_image, task["prompt"], **task["params"])
             else:
                 raise Exception("Function not found")
     
