639b7e951236cf39b038fec93cf49cfcca421616
shashank2000 <46056525+shashank2000@users.noreply.github.com>
2023-05-24 14:02:59 -0700

    Added `Layer` class, test suite, operators dictionary, misc decorators (#1)
    * renamed to operators
    
    * added layer logic, testing skeleton
    
    * added testing, README, gitignore, more fleshed out
    
    * added np
    
    * fixed operator precedence issue
    
    * fixed operator precedence

--- opendream/opendream.py
+++ opendream/opendream.py
@@ -1,46 +1,58 @@
 import json
-
+from . import reference 
 # we need to represent the execution engine here
 
-functions = {}
-def dream(dream_func):
-    # FLESH OUT PARAM CHECKING, THIS NEEDS TO MATCH SO EXEC ENGINE CAN RUN
-    if dream_func.__code__.co_varnames[0] != "prompt":
-        raise Exception("dream function must have prompt as first argument")
-    functions["dream"] = dream_func
-    
-def mask_and_inpaint(mask_and_inpaint_func):
-    # FLESH OUT PARAM CHECKING, THIS NEEDS TO MATCH SO EXEC ENGINE CAN RUN
-    functions["mask_and_inpaint"] = mask_and_inpaint_func
+operators = {}
 
+# TODO: all decorators must return Layer objects
+# decorator to make a function into a dream operator
+def operator(func):
+    operators[func.__name__] = func
+    return func
+
+@operator
+def dream(prompt: str, model_ckpt: str = "runwayml/stable-diffusion-v1-5", seed: int = 42, device: str = "mps", batch_size: int = 1, selected: int = 0, num_steps: int = 20, guidance_scale: float = 7.5, **kwargs):
+    return reference.dream(prompt, model_ckpt, seed, device, batch_size, selected, num_steps, guidance_scale, **kwargs)
+
+@operator
+def mask_and_inpaint(mask_image, image, prompt, model_ckpt, seed: int = 42, device: str = "mps", batch_size: int = 1, selected: int = 0, num_steps: int = 20, guidance_scale: float = 7.5, **kwargs):
+    return reference.mask_and_inpaint(mask_image, image, prompt, model_ckpt, seed, device, batch_size, selected, num_steps, guidance_scale, **kwargs)
+
+@operator
 def make_dummy_mask():
-    from PIL import Image, ImageDraw
+    return reference.make_dummy_mask()
 
-    # Create a blank mask with the size of 512x512
-    width, height = 512, 512
-    mask = Image.new("1", (width, height))
 
-    # Draw a simple shape on the mask using ImageDraw
-    draw = ImageDraw.Draw(mask)
-    draw.rectangle([128, 128, 384, 384], fill="white")
-    
-    mask.save(f"mask_inpaint.png")
-                
-    return mask
+# populate the operators dictionary 
+global_vars = globals().copy()
 
+# Iterate over global variables
+for var_name, var_value in global_vars.items():
+    # Check if the variable is a function decorated with @operator
+    if callable(var_value) and hasattr(var_value, '__name__') and var_value.__name__ != operator.__name__:
+        operators[var_value.__name__] = var_value
 
 def execute(json_file_path: str):
+    # layers might have to be a global variable for delete to work
+    layers = []
+
+    # override operators as necessary
+    globals_vars = globals().copy()
+
+    # Iterate over global variables
+    for var_name, var_value in globals_vars.items():
+        # Check if the variable is a function decorated with @operator
+        if callable(var_value) and hasattr(var_value, '__name__') and var_value.__name__ != operator.__name__ and hasattr(var_value, '__annotations__'):
+            operators[var_value.__name__] = var_value
+
     with open(json_file_path) as json_file:
         data = json.load(json_file)
         workflow = data["workflow"]
-        current_image = None
         for task in workflow:
-            if task["operation"] == "dream":
-                current_image = functions["dream"](task["prompt"], **task["params"])
-            elif task["operation"] == "mask_and_inpaint":
-                # print("OK")
-                current_image = functions["mask_and_inpaint"](make_dummy_mask(), current_image, task["prompt"], **task["params"])
-            else:
-                raise Exception("Function not found")
-    
-    return current_image
+            try:
+                layers.append(operators[task["operation"]](**task["params"]))
+            except Exception as e:
+                print(f"Error executing {task['operation']}: {e}")
+                raise e
+
+    return layers
---
+++ opendream/reference.py
@@ -0,0 +1,50 @@
+'''
+Reference implementation of various functions. The reason this exists
+is so that the user can choose to either stick with the 
+'''
+
+import numpy as np
+from diffusers import StableDiffusionPipeline, StableDiffusionPipeline, StableDiffusionInpaintPipeline
+import torch
+from PIL import Image
+
+def dream(prompt: str, model_ckpt: str = "runwayml/stable-diffusion-v1-5", seed: int = 42, device: str = "mps", batch_size: int = 1, selected: int = 0, num_steps: int = 20, guidance_scale: float = 7.5, **kwargs):
+    pipe = StableDiffusionPipeline.from_pretrained(model_ckpt, torch_dtype=torch.float32, safety_checker=None)
+    pipe = pipe.to(device)
+    
+    generator = [torch.Generator().manual_seed(seed + i) for i in range(batch_size)]
+    
+    image = pipe(prompt, generator=generator, num_inference_steps=num_steps, guidance_scale=guidance_scale).images[selected]
+
+    image.save(f"{prompt}_{seed}.png")
+    return image
+
+
+def mask_and_inpaint(mask_image: Image.Image, image: Image.Image, prompt: str, model_ckpt: str = "runwayml/stable-diffusion-inpainting", seed: int = 42, device: str = "mps", batch_size: int = 1, selected: int = 0, num_steps: int = 20, guidance_scale: float = 7.5, **kwargs):
+    pipe = StableDiffusionInpaintPipeline.from_pretrained(
+        model_ckpt,
+        safety_checker=None,
+    )
+    pipe = pipe.to(device)
+    
+    generator = [torch.Generator().manual_seed(seed + i) for i in range(batch_size)]
+    
+    inpainted_image = pipe(prompt=prompt, image=image, mask_image=mask_image, generator=generator, num_inference_steps=num_steps, guidance_scale=guidance_scale).images[selected]
+    inpainted_image.save(f"{prompt}_inpaint.png")
+    return inpainted_image
+
+
+def make_dummy_mask():
+    from PIL import Image, ImageDraw
+
+    # Create a blank mask with the size of 512x512
+    width, height = 512, 512
+    mask = Image.new("1", (width, height))
+
+    # Draw a simple shape on the mask using ImageDraw
+    draw = ImageDraw.Draw(mask)
+    draw.rectangle([128, 128, 384, 384], fill="white")
+    
+    mask.save(f"mask_inpaint.png")
+                
+    return mask
