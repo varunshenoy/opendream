fede6e9bd9bd68a45fb57b92b162c5f5bd8d876f
shashank2000 <sr22@stanford.edu>
2023-05-23 19:47:08 -0700

    added layer logic, testing skeleton

--- opendream/opendream.py
+++ opendream/opendream.py
@@ -1,46 +1,36 @@
 import json
-
+import reference
 # we need to represent the execution engine here
 
 operators = {}
-def dream(dream_func):
-    # FLESH OUT PARAM CHECKING, THIS NEEDS TO MATCH SO EXEC ENGINE CAN RUN
-    if dream_func.__code__.co_varnames[0] != "prompt":
-        raise Exception("dream function must have prompt as first argument")
-    operators["dream"] = dream_func
-    
-def mask_and_inpaint(mask_and_inpaint_func):
-    # FLESH OUT PARAM CHECKING, THIS NEEDS TO MATCH SO EXEC ENGINE CAN RUN
-    operators["mask_and_inpaint"] = mask_and_inpaint_func
 
+# decorator to make a function into a dream operator
+def operator(func):
+    operators[func.__name__] = func
+    return func
+
+@operator
+def dream(prompt: str, model_ckpt: str = "runwayml/stable-diffusion-v1-5", seed: int = 42, device: str = "mps", batch_size: int = 1, selected: int = 0, num_steps: int = 20, guidance_scale: float = 7.5, **kwargs):
+    return reference.dream(prompt, model_ckpt, seed, device, batch_size, selected, num_steps, guidance_scale, **kwargs)
+
+@operator
+def mask_and_inpaint(mask_image: Image.Image, image: Image.Image, prompt: str, model_ckpt: str = "runwayml/stable-diffusion-inpainting", seed: int = 42, device: str = "mps", batch_size: int = 1, selected: int = 0, num_steps: int = 20, guidance_scale: float = 7.5, **kwargs):
+    return reference.mask_and_inpaint(mask_image, image, prompt, model_ckpt, seed, device, batch_size, selected, num_steps, guidance_scale, **kwargs)
+
+@operator
 def make_dummy_mask():
-    from PIL import Image, ImageDraw
-
-    # Create a blank mask with the size of 512x512
-    width, height = 512, 512
-    mask = Image.new("1", (width, height))
-
-    # Draw a simple shape on the mask using ImageDraw
-    draw = ImageDraw.Draw(mask)
-    draw.rectangle([128, 128, 384, 384], fill="white")
-    
-    mask.save(f"mask_inpaint.png")
-                
-    return mask
-
+    return reference.make_dummy_mask()
 
 def execute(json_file_path: str):
+    layers = []
     with open(json_file_path) as json_file:
         data = json.load(json_file)
         workflow = data["workflow"]
-        current_image = None
         for task in workflow:
-            if task["operation"] == "dream":
-                current_image = operators["dream"](task["prompt"], **task["params"])
-            elif task["operation"] == "mask_and_inpaint":
-                # print("OK")
-                current_image = operators["mask_and_inpaint"](make_dummy_mask(), current_image, task["prompt"], **task["params"])
-            else:
-                raise Exception("Function not found")
-    
-    return current_image
+            try:
+                layers.append(operators[task["operation"]](**task["params"]))
+            except Exception as e:
+                print(f"Error executing {task['operation']}: {e}")
+                raise e
+
+    return layers
