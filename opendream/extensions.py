361cac3091cf1e054aec68be409333fe1832271d
Varun Shenoy <varun.nshenoy@gmail.com>
2023-06-05 09:09:51 -0700

    remove .ds_store + add dalle dream

--- opendream/extensions.py
+++ opendream/extensions.py
@@ -1,10 +1,27 @@
 from opendream import opendream
+from opendream.layer import Layer
 from PIL import Image
 
 # USERS CAN ADD THEIR OWN OPERATIONS HERE
 
-@opendream.define_op
-def a_test_operation(test: str):
-    width, height = 512, 512 
-    dummy_image = Image.new("1", (width, height))
-    return opendream.Layer(dummy_image)
+# @opendream.define_op
+# def a_test_operation(test: str):
+#     width, height = 512, 512 
+#     dummy_image = Image.new("1", (width, height))
+#     return opendream.Layer(dummy_image)
+
+# @opendream.define_op
+# def dream(prompt: str):
+#     import openai
+#     import os
+    
+#     openai.api_key = os.environ["OPENAI_API_KEY"]
+    
+#     response = openai.Image.create(
+#         prompt=prompt,
+#         n=1,
+#         size="512x512",
+#     )
+#     image_url = response['data'][0]['url']
+    
+#     return Layer.from_url(image_url)
