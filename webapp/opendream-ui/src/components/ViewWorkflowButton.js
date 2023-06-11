1e81a3e5a077c9399f36df081dcdc9fe4d423d75
Varun Shenoy <varun.nshenoy@gmail.com>
2023-06-10 17:32:57 -0700

    clean up ui for viewing workflow

--- webapp/opendream-ui/src/components/ViewWorkflowButton.js
+++ webapp/opendream-ui/src/components/ViewWorkflowButton.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
-import { ScrollText } from "lucide-react";
-import { Modal } from "antd";
+import { ScrollText, Download } from "lucide-react";
+import { Modal, Card } from "antd";
 
 // TODO: have this show the actual workflow 
 const ViewWorkflowButton = ({currentState}) => {
@@ -18,25 +18,60 @@ const ViewWorkflowButton = ({currentState}) => {
         footer={[]}
       >
         {currentState && currentState.map((layer, index) => {
-          return <div key={index} class="flex flex-col py-2">
-            <span class="font-bold text-base">Layer {layer.id} - {layer.metadata.op}</span>
-            {layer.metadata.params && <>
-              <span class="font-bold">Parameters:</span>
-              <div class="flex flex-col">{layer.metadata.params.map(param => (
-                <span class="pl-8">{param}</span>
-              ))}</div>
-            </>}
-            {layer.metadata.options && Object.keys(layer.metadata.options).length > 0 && <>
-              <span class="font-bold">Options:</span>
-              <div class="flex flex-col">{Object.entries(layer.metadata.options).map(([key, value]) => (
-                <span class="pl-8"><span class="underline">{key}</span>: {value}</span>
-              ))}</div>
-            </>}
-          </div>
+          return (
+            <Card
+              size="small"
+              title={"Layer " + (layer.id).toString()}
+              className="mb-4"
+            >
+              <code>OPERATION: <code className="p-1 bg-slate-100 rounded-md">{layer.metadata.op}</code></code>
+                {layer.metadata.params && <div class="pt-2 pb-2">
+                  <code className="font-lg">PARAMETERS:</code>
+                  <div class="flex flex-col">{layer.metadata.params.map(param => (
+                    <span class="pl-2"><code className="ml-2 p-1 bg-slate-100 rounded-md">{param}</code></span>
+                  ))}</div>
+                </div>}
+                {layer.metadata.options && Object.keys(layer.metadata.options).length > 0 && <div class="pb-2">
+                  <code>OPTIONS:</code>
+                  <div class="flex flex-col">{Object.entries(layer.metadata.options).map(([key, value]) => (
+                    <span class="pl-2"><span className="text-gray-500 ">{key}:</span><code className="ml-2 p-1 bg-slate-100 rounded-md">{value}</code></span>
+                  ))}</div>
+                </div>}
+            </Card>
+            // <div key={index} class="flex flex-col py-2">
+            //   <span class="font-bold text-base">Layer {layer.id} - {layer.metadata.op}</span>
+              // {layer.metadata.params && <>
+              //   <span class="font-bold">Parameters:</span>
+              //   <div class="flex flex-col">{layer.metadata.params.map(param => (
+              //     <span class="pl-8">{param}</span>
+              //   ))}</div>
+              // </>}
+              // {layer.metadata.options && Object.keys(layer.metadata.options).length > 0 && <>
+              //   <span class="font-bold">Options:</span>
+              //   <div class="flex flex-col">{Object.entries(layer.metadata.options).map(([key, value]) => (
+              //     <span class="pl-8"><span class="underline">{key}</span>: {value}</span>
+              //   ))}</div>
+              // </>}
+            // </div>
+          )
         })}
+ 
+        <a
+          href={`data:text/json;charset=utf-8,${encodeURIComponent(JSON.stringify(currentState))}`}
+          download="workflow.json"
+          className="flex hover:text-white rounded-md bg-zinc-900 w-full px-3.5 py-2.5 my-4 text-sm font-semibold text-white shadow-sm hover:bg-zinc-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-zinc-600"
+        >
+          Download Workflow <Download size={18} class="ml-2"></Download>
+        </a>
       </Modal>
       <a
-        onClick={setIsModalOpen}
+        onClick={() => {
+          if (currentState) {
+            setIsModalOpen(true)
+          } else {
+            alert("No workflow to view!")
+          }
+        }}
         className="flex items-center justify-center rounded-md bg-zinc-900 w-full px-3.5 py-2.5 my-4 text-sm font-semibold text-white shadow-sm hover:bg-zinc-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-zinc-600"
       >
         View Workflow
